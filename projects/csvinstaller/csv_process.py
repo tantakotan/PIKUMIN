0fa08a7d8d303fad9796268329dac731b98cf85a
tantakotan <planettako@gmail.com>
2018-12-22 16:10:45 +0900

    20181222r2

--- projects/csvinstaller/csv_process.py
+++ projects/csvinstaller/csv_process.py
@@ -32,24 +32,44 @@ def get_dictionary(arg1, arg2, arg3):
     return dev_dict
 
 
-def check_index(arg1, arg2):
-    with open(arg1, 'r', encoding='utf-8_sig') as f:
+def get_index(path_of_csv):
+    with open(path_of_csv, 'r', encoding='utf-8_sig') as f:
         index_of_index = csv.reader(f, delimiter=',')
         list_of_index = next(index_of_index)
-        count_index = [i for i, x in enumerate(list_of_index) if x == arg2]
-        print(count_index)
 
-        if arg2 in list_of_index:
-            print('index check...OK: ' + arg2)
-        else:
-            print('index check...NG: ' + arg2)
-            exit()
+    return list_of_index
 
-        if len(count_index) < 2:
-            print('index count check...OK: ' + str(count_index))
-        else:
-            print('index count check...NG: ' + arg2 + ' is ' + str(count_index) + ' duplicated')
-            exit()
+
+def check_index(path_of_csv, index_of_hoge):
+    with open(path_of_csv, 'r', encoding='utf-8_sig') as f:
+
+        index_of_index = csv.reader(f, delimiter=',')
+        list_of_index = next(index_of_index)
+
+        if type(index_of_hoge) is str:
+            index_of_hoge = index_of_hoge.replace(' ', '')
+            list_of_hoge = index_of_hoge.split(',')
+        elif type(index_of_hoge) is list:
+            list_of_hoge = index_of_hoge
+
+        i2 = 0
+
+        while i2 < len(list_of_hoge):
+
+            count_index = [i for i, x in enumerate(list_of_index) if x == list_of_hoge[i2]]
+
+            if list_of_hoge[i2] in list_of_index:
+                print('index check...OK: ' + list_of_hoge[i2])
+                i2 += 1
+            else:
+                print('index check...NG: ' + list_of_hoge[i2])
+                exit()
+
+            if len(count_index) < 2:
+                print('index count check...OK: ' + str(count_index))
+            else:
+                print('index count check...NG: ' + list_of_hoge + ' is ' + str(count_index) + ' duplicated')
+                exit()
 
 
 def check_path(path):
--- projects/starter_csv.py
+++ projects/starter_csv.py
@@ -10,7 +10,7 @@ def starter_nw(dict_of_ini):
     path_of_csv = os.path.join(dict_of_ini['csv']['csv_folder'], dict_of_ini['csv']['csv_file'])
     path_of_tpl = os.path.join(dict_of_ini['tpl']['tpl_folder'])
     index_of_tpl = dict_of_ini['tpl']['tpl_index']
-    index_of_nw = dict_of_ini['nw']['nw_indexs']
+    index_of_nw = dict_of_ini['nw']['nw_index']
     index_of_conf = dict_of_ini['nw']['config_index']
     flag_of_nw = strtobool(dict_of_ini['nw']['nw_index_all'])
 
@@ -19,8 +19,14 @@ def starter_nw(dict_of_ini):
 
     csv_process.check_index(path_of_csv, index_of_tpl)
     csv_process.check_index(path_of_csv, index_of_conf)
-    csv_process.check_index(path_of_csv, index_of_nw)
-    csv_process.check_index(path_of_csv, index_of_nw)
+
+    if flag_of_nw:
+        list_of_index = csv_process.get_index(path_of_csv)
+        list_of_index.remove(index_of_tpl)
+        list_of_index.remove(index_of_conf)
+        csv_process.check_index(path_of_csv, list_of_index)
+    else:
+        csv_process.check_index(path_of_csv, index_of_nw)
 
     list_of_tpl = csv_process.get_columns(path_of_csv, index_of_tpl)
     dict_of_nw = csv_process.get_dictionary(path_of_csv, index_of_conf, index_of_nw)
